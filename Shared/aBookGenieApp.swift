140eb43f7c826a879fc47f1396cbe84d52c4971f
John Varney <john@jdvarney.com>
2021-11-28 12:20:06 -0500

    Initial Commit

---
+++ Shared/aBookGenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  aBookGenieApp.swift
+//  Shared
+//
+//  Created by John on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct aBookGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
